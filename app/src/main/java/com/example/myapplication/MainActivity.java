989cac0a63265889dea6ac55c71a7c7fe4a97be3
zyeooon <zyeooon@naver.com>
2020-12-08 14:32:27 +0900

    MainActivity login기능 구현 + Internet권한과 apache, textclear어쩌구 추가 by 지현

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -9,12 +10,41 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.StrictMode;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewFlipper Vf;
+    Button btn_log;
+    EditText msg_id, msg_pw;
+    HttpPost httppost;
+    StringBuffer buffer;
+    HttpClient httpclient;
+    HttpResponse response;
+    List<NameValuePair> nameValuePairs;
+    ProgressDialog dialog = null;
+    TextView tv; //welcome 부분
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +53,28 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        btn_log = (Button) findViewById(R.id.btn_log);
+        msg_id = (EditText) findViewById(R.id.msg_id);
+        msg_pw = (EditText) findViewById(R.id.msg_pw);
+        tv=(TextView)findViewById(R.id.textView);
+
         Button b = (Button)findViewById(R.id.btn_log);
         b.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Intent intent = new Intent(
-                        getApplicationContext(),
-                        TodayActivity.class);
-                startActivity(intent);
+
+                dialog = ProgressDialog.show(MainActivity.this, "",
+                        "Validating user...", true);
+                new Thread(new Runnable() {
+                    public void run() {
+                        login();
+                    }
+                }).start();
+
+//                Intent intent = new Intent(
+//                        getApplicationContext(),
+//                        TodayActivity.class);
+//                startActivity(intent);
             }
         });
 //        화면전환 인텐드 추가 내용 - 지현
@@ -59,5 +103,48 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    void login() {
+        try {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitNetwork().build());
+            httpclient = new DefaultHttpClient();
+            httppost = new HttpPost("http://192.168.219.184/proflogin.php"); //ip주소변경
+            nameValuePairs = new ArrayList<NameValuePair>(2);
+            String id = msg_id.getText().toString();
+            nameValuePairs.add(new BasicNameValuePair("username", msg_id.getText().toString()));
+            nameValuePairs.add(new BasicNameValuePair("password", msg_pw.getText().toString()));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            response = httpclient.execute(httppost);
+            ResponseHandler<String> responseHandler = new BasicResponseHandler();
+            final String response = httpclient.execute(httppost, responseHandler);
+            System.out.println("Response : " + response);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    tv.setText("Response from PHP : " + response);
+                    dialog.dismiss();
+                }
+            });
 
+            if (response.equalsIgnoreCase("User Found")) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                Intent myIntent = new Intent(this, TodayActivity.class);
+                myIntent.putExtra("id",id);
+                startActivity(myIntent);
+                //startActivity((new Intent(MainActivity.this, TodayActivity.class)));
+                //finish();
+            } else {
+                Toast.makeText(MainActivity.this, "Login Fail", Toast.LENGTH_SHORT).show();
+            }
+        }
+        catch(Exception e)
+        {
+            dialog.dismiss();
+            System.out.println("Exception : " + e.getMessage());
+        }
+    }
 }
