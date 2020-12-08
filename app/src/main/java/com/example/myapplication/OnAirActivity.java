6d0759b5975a2ae6256ce34f941440a31241aa48
zyeooon <zyeooon@naver.com>
2020-12-08 18:28:49 +0900

    OnAirActivity 변경 데이터 불러오기 코드 by 지현

--- app/src/main/java/com/example/myapplication/OnAirActivity.java
+++ app/src/main/java/com/example/myapplication/OnAirActivity.java
@@ -1,17 +1,42 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class OnAirActivity extends AppCompatActivity {
+
+    String myJSON;
+    private static final String TAG_RESULTS = "result";
+    private static final String TAG_NO = "SUBno";
+    private static final String TAG_NAME = "SUBname";
+
+    JSONArray peoples = null;
+    ArrayList<HashMap<String, String>> personList;
+    ListView list;
+    ////jihyeon
 
     ListView studenttable2;
     String[] stuNo ={"1","2","3"};
@@ -28,10 +53,13 @@ public class OnAirActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        studenttable2=findViewById(R.id.studenttable2);
-        OnAirAdapter OnAirAdapter=new OnAirAdapter(this, stuNo,stuName,butO,butA,butX);
-        studenttable2.setAdapter(OnAirAdapter);
+//        studenttable2=findViewById(R.id.studenttable2);
+//        OnAirAdapter OnAirAdapter=new OnAirAdapter(this, stuNo,stuName,butO,butA,butX);
+//        studenttable2.setAdapter(OnAirAdapter);
         //추가
+        list = (ListView) findViewById(R.id.studenttable2);
+        personList = new ArrayList<HashMap<String, String>>();
+        getData("http://192.168.219.184/profonair.php"); //ip주소변경
 
         Button b = (Button)findViewById(R.id.btn_change);
         b.setOnClickListener(new View.OnClickListener(){
@@ -84,5 +112,85 @@ public class OnAirActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    protected void showList() {
+        try {
+            JSONObject jsonObj = new JSONObject(myJSON);
+            peoples = jsonObj.getJSONArray(TAG_RESULTS);
+
+            for (int i = 0; i < peoples.length(); i++) {
+                JSONObject c = peoples.getJSONObject(i);
+                String SUBno = c.getString(TAG_NO);
+                String SUBname = c.getString(TAG_NAME);
+
+                HashMap<String, String> persons = new HashMap<String, String>();
+
+                persons.put(TAG_NO, SUBno);
+                persons.put(TAG_NAME, SUBname);
+
+
+                personList.add(persons);
+            }
+
+            ListAdapter adapter = new SimpleAdapter(
+                    OnAirActivity.this, personList, R.layout.onairlist,
+                    new String[]{TAG_NO, TAG_NAME},
+                    new int[]{R.id.txt_name2, R.id.textOX}
+            );
+
+            list.setAdapter(adapter);
+            list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView parent, View v, int position, long id) {
+                    Intent intent = new Intent(getApplicationContext(),Sub1Activity.class);
+                    startActivity(intent);
+                }
+            });
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+    private void getData(String url) {
+        class GetDataJSON extends AsyncTask<String, Void, String> {
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                String uri = params[0];
+
+                BufferedReader bufferedReader = null;
+                try {
+                    URL url = new URL(uri);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+
+                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    String json;
+                    while ((json = bufferedReader.readLine()) != null) {
+                        sb.append(json + "\n");
+                    }
+
+                    return sb.toString().trim();
+
+                } catch (Exception e) {
+                    return null;
+                }
+
+
+            }
+
+
+            @Override
+            protected void onPostExecute(String result) {
+                myJSON = result;
+                showList();
+            }
+        }
+        GetDataJSON g = new GetDataJSON();
+        g.execute(url);
     }
 }
--- app/src/main/java/com/example/myapplication/OnAirData.java
+++ app/src/main/java/com/example/myapplication/OnAirData.java
@@ -1,23 +1,23 @@
-package com.example.myapplication;
-
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-public class OnAirData {
-    TextView stu_no;
-    TextView stu_name;
-    ImageView btn_o;
-    ImageView btn_a;
-    ImageView btn_x;
-    OnAirData(View v)
-    {
-        stu_no = v.findViewById(R.id.txt_no2);
-        stu_name = v.findViewById(R.id.txt_name2);
-        btn_o=v.findViewById(R.id.btn_O2);
-        btn_a=v.findViewById(R.id.btn_A2);
-        btn_x=v.findViewById(R.id.btn_X2);
-
-
-    }
-}
+//package com.example.myapplication;
+//
+//import android.view.View;
+//import android.widget.ImageView;
+//import android.widget.TextView;
+//
+//public class OnAirData {
+//    TextView stu_no;
+//    TextView stu_name;
+//    ImageView btn_o;
+//    ImageView btn_a;
+//    ImageView btn_x;
+//    OnAirData(View v)
+//    {
+//        stu_no = v.findViewById(R.id.txt_no2);
+//        stu_name = v.findViewById(R.id.txt_name2);
+//        btn_o=v.findViewById(R.id.btn_O2);
+//        btn_a=v.findViewById(R.id.btn_A2);
+//        btn_x=v.findViewById(R.id.btn_X2);
+//
+//
+//    }
+//}
